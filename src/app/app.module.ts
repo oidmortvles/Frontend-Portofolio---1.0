db0c28f06805f80a3c13bd296b960c725e934539
Daniel Pereyra <danielper.dev@gmail.com>
2022-08-13 15:01:14 -0300

    Version 1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    SobreMiComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    InicioComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
